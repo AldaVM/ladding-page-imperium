eab7a9009a3eae6e982aa1c24351653d21568285
AldairVM <amvalencia-avatar@global.com>
2021-10-10 19:23:46 -0500

    cambio de configuracion para cargar custom head

--- pages/_app.js
+++ pages/_app.js
@@ -9,18 +9,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-        <link
-          href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
-          rel="stylesheet"
-        />
-        <link rel="icon" type="image/png" href="favicon.ico" />
-        <meta name="author" content="Aldair Valencia <aldairvm95@gmail>" />
-        <meta name="keywords" content="Imperium Cross WebSite" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-        <meta property="og:title" content="Imperium Cross WebSite" />
-        <meta property="og:image" content="/assets/logo-imperium-cross.png" />
       </Head>
       <Component {...pageProps} />;
     </>
--- pages/_document.js
+++ pages/_document.js
@@ -1,4 +1,4 @@
-import Document, { Html, Main, NextScript } from "next/document";
+import Document, { Html, Head, Main, NextScript } from "next/document";
 import { ServerStyleSheet } from "styled-components";
 
 class MyDocument extends Document {
@@ -32,6 +32,20 @@ class MyDocument extends Document {
   render() {
     return (
       <Html lang="es">
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@400;700&display=swap"
+            rel="stylesheet"
+          />
+          <link rel="icon" type="image/png" href="favicon.ico" />
+          <meta name="author" content="Aldair Valencia <aldairvm95@gmail>" />
+          <meta name="keywords" content="Imperium Cross WebSite" />
+
+          <meta property="og:title" content="Imperium Cross WebSite" />
+          <meta property="og:image" content="/assets/logo-imperium-cross.png" />
+        </Head>
         <body>
           <Main />
           <NextScript />
